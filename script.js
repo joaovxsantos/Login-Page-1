764d2d5e85dc859f05db4f8c8421914ebc8120cb
João Vitor <joaovitor55670@gmail.com>
2023-05-10 17:27:59 -0300

    system page em andamento

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const password = document.querySelector('#password');
 submit.addEventListener('click', () => {
     new Promise((resolve, reject) => {
 
-        if (!user.value || !password.value) {
-            reject(`Preencha todos os campos!`);
-        } else {
+        if (user.value == 'João' && password.value == '123') {
             resolve('Login feito com sucesso!', user.value = '', password.value = '')
+            window.location.href = './plataform.html'
+        } else {
+            reject(`ERRO`);
         }
+
     }).then(ok => console.log(ok))
         .catch(err => console.log(err))
 })
