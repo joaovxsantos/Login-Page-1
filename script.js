437a6d0097517c31d010cb2d96ea2f078d9cadf1
João Vitor <joaovitor55670@gmail.com>
2023-08-28 16:50:40 -0300

    ok

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const submit = document.querySelector('#submit');
 const user = document.querySelector('#user');
 const password = document.querySelector('#password');
+const body = document.querySelector('body');
 
+const adicionarAlerta = (mensagem) => {
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('alert', 'border', 'mt-3', 'text-black', 'alert-dismissible', 'fade', 'show');
+    alertDiv.innerHTML = `
+        <p>${mensagem}</p>
+        <button class="btn-close" data-bs-dismiss="alert"></button>
+    `;
+    body.appendChild(alertDiv);
+}
+
+const verificarAlert = () => {
+    return body.querySelector('.alert');
+}
 
 submit.addEventListener('click', () => {
-    new Promise((resolve, reject) => {
-
-        if (user.value == 'João' && password.value == '123') {
-            resolve('Login feito com sucesso!', user.value = '', password.value = '')
-            window.location.href = './plataform.html'
-        } else {
-            reject(`ERRO`);
+    if (!user.value || !password.value) {
+        if (!verificarAlert()) {
+            adicionarAlerta('Preencha os dados de forma correta.');
         }
-
-    }).then(ok => console.log(ok))
-        .catch(err => console.log(err))
-})
-
+    } else {
+        body.innerHTML = `<h1>Login efetuado com sucesso!</h1>`
+    }
+});
